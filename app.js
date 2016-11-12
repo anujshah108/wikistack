10800fc1f15d6f8040a3676a3139c6e25efedc8f
anujshah108 <ashah812@gmail.com>
2016-11-12 09:57:12 -0500

    upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const nunjucks = require('nunjucks')
+const sequelize = require('sequelize')
+const models = require('./models');
+const server = app.listen(3001, function(){console.log('server listening')});
+const wikiRouter = require('./routes/wiki');
+
+
+app.use(express.static('public'));
+app.use(bodyparser)
+app.use(bodyparser.json())
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    server.listen(3001, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
+
+app.use('/wiki', wikiRouter);
+
+
+
---
+++ routes/wiki.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.send('got to GET /wiki/');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('got to POST /wiki/');
+});
+
+router.get('/add', function(req, res, next) {
+  res.send('got to GET /wiki/add');
+});
+
+
+module.exports = router;
