dd1efcfdf6acf9e64176ae1b5c15282ed69be740
anujshah108 <ashah812@gmail.com>
2016-11-14 12:53:08 -0500

    finished routes

--- models/index.js
+++ models/index.js
@@ -24,10 +24,23 @@ var Page = db.define('page', {
         defaultValue: Sequelize.NOW
     }
 }, {
-  instanceMethods: {
+  hooks: {
+  	   beforeValidate: function(page){
+					  if (page.title) {
+					    // Removes all non-alphanumeric characters from title
+					    // And make whitespace underscore
+					    page.urlTitle = page.title.replace(/\s+/g, '_').replace(/\W/g, '');
+						  } else {
+						    // Generates random 5 letter string
+						    page.urlTitle = Math.random().toString(36).substring(2, 7);
+						  }
+						}
+					},
+	instanceMethods: {
     route: function() {
       return '/wiki/'+ this.urlTitle
     }
+
   }
 })
 
@@ -44,6 +57,8 @@ var User = db.define('user', {
   
 })
 
+// Page.sync({force: true});
+// User.sync({force: true});
 
 module.exports = {
   Page: Page,
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,9 +1,22 @@
 const express = require('express')
 var router = express.Router();
+var models = require('../models');
+var Page = models.Page; 
+var User = models.User; 
+
 
 
 router.get('/', function(req, res, next) {
-  res.send('got to GET /wiki/');
+	Page.findAll({
+}).then(
+function(allpages){ 
+	console.log(allpages)
+	res.render('index',
+	{
+	pages: allpages
+	})
+}	
+)
 });
 
 // wikiRouter.get('/add', function(req, res, next) {
@@ -19,9 +32,39 @@ router.get('/', function(req, res, next) {
   res.redirect('/');
 });
 
+router.get('/:urlTitle', function (req, res, next) {
+
+  Page.findOne({ 
+    where: { 
+      urlTitle: req.params.urlTitle 
+    } 
+  })
+  .then(function(page){
+    res.render('wikipage', {
+    	title: page.title,
+    	content: page.content
+    })
+  })
+  .catch(next);
+
+});
+
+
 router.post('/', function(req, res, next) {
-console.log(req.body)
-  res.json(req.body);
+
+  // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+  var page = Page.build({
+    title: req.body.title,
+    content: req.body.content
+  });
+  // STUDENT ASSIGNMENT:
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise or it can take a callback.
+  page.save()
+  	.then(function(val){
+  		res.redirect(val.route())
+  	})
 });
 
 module.exports = router;
